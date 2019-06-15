84ff7b88b04350d9cc183a41ba7c608096ad3d3d
spencerng <sng1488@gmail.com>
2019-06-15 14:30:34 -0400

    Add title fade; format HTML/CSS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var startButton = document.getElementById("startButton");
 var gamePane = document.getElementById("gamePane"); 
 var mainMenu = document.getElementById("mainMenu")
-
+var title = document.getElementById("title")
 
 //Initialize function
 startButton.onclick=function(){
@@ -11,6 +11,10 @@ startButton.onclick=function(){
 	$(gamePane).animate({
 		height: 640
 	}, 600);
+
+	$(title).fadeOut(function(){
+		$(this).html("Choose Your Character").fadeIn();
+	});
 	
 	$(mainMenu).fadeIn(500);
 };

b7d9ab9cd0d29b5f6f890ccbc271000c38f0cdd0
spencerng <sng1488@gmail.com>
2019-06-15 17:34:39 -0400

    Add title to Google Slides card

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var startButton = document.getElementById("startButton");
 var gamePane = document.getElementById("gamePane"); 
 var mainMenu = document.getElementById("mainMenu")
 var title = document.getElementById("title")
+var charButtons = [document.getElementById("ormaButton"), document.getElementById("darleneButton"), document.getElementById("charThreeButton"), document.getElementById("charFourButton")]
 
 //Initialize function
 startButton.onclick=function(){
@@ -12,9 +13,21 @@ startButton.onclick=function(){
 		height: 640
 	}, 600);
 
-	$(title).fadeOut(function(){
-		$(this).html("Choose Your Character").fadeIn();
-	});
+	setTitle("Choose Your Character")
 	
 	$(mainMenu).fadeIn(500);
 };
+
+for(var i = 0; i < charButtons.length; i++){
+	charButtons[i].onclick=function(){
+		$(mainMenu).fadeOut();
+		console.log("Clicked");
+	};
+}
+
+
+function setTitle(titleWords){
+	$(title).fadeOut(function(){
+		$(this).html(titleWords).fadeIn();
+	});
+}

5d819bbae815f490a2f087ce5daeedb8a35538a6
spencerng <sng1488@gmail.com>
2019-06-15 20:52:06 -0400

    Begin work on status screen

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var startButton = document.getElementById("startButton");
 var gamePane = document.getElementById("gamePane"); 
 var mainMenu = document.getElementById("mainMenu")
+var statusScreen = document.getElementById("statusScreen")
 var title = document.getElementById("title")
 var charButtons = [document.getElementById("ormaButton"), document.getElementById("darleneButton"), document.getElementById("charThreeButton"), document.getElementById("charFourButton")]
 
@@ -10,7 +11,7 @@ startButton.onclick=function(){
 	$(this).fadeOut();
 	
 	$(gamePane).animate({
-		height: 640
+		height: 650
 	}, 600);
 
 	setTitle("Choose Your Character")
@@ -21,7 +22,8 @@ startButton.onclick=function(){
 for(var i = 0; i < charButtons.length; i++){
 	charButtons[i].onclick=function(){
 		$(mainMenu).fadeOut();
-		console.log("Clicked");
+		statusScreen.style.display = "block";
+		$(status).fadeIn();
 	};
 }
 

579879c847f0a3bf399c313733e0078b2922af88
spencerng <sng1488@gmail.com>
2019-06-17 22:01:49 -0400

    Try different CSV to array function

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var statusScreen = document.getElementById("statusScreen");
 var title = document.getElementById("title");
 var charButtons = [document.getElementById("ormaButton"), document.getElementById("darleneButton"), document.getElementById("charThreeButton"), document.getElementById("charFourButton")];
 var homeButton = document.getElementById("homeButton");
-var homeButtonResult = document.getElementById("homeButtonResult");
 
 //Status menu
 var personImg = document.getElementById("personImg");
@@ -84,25 +83,66 @@ function advanceMonth(charState) {
 	charState.month += 1;
 }
 
-function CSVtoArray(text) {
-	var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
-	var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
-	// Return NULL if input string is not well formed CSV string.
-	if (!re_valid.test(text)) return null;
-	var a = []; // Initialize array to receive values.
-	text.replace(re_value, // "Walk" the string using replace with callback.
-		function(m0, m1, m2, m3) {
-			// Remove backslash from \' in single quoted values.
-			if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
-			// Remove backslash from \" in double quoted values.
-			else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
-			else if (m3 !== undefined) a.push(m3);
-			return ''; // Return empty string.
-		});
-	// Handle special case of empty last value.
-	if (/,\s*$/.test(text)) a.push('');
-	return a;
-};
+function CSVtoArray( strData, strDelimiter ){
+        // Check to see if the delimiter is defined. If not,
+        // then default to comma.
+        strDelimiter = (strDelimiter || ",");
+        // Create a regular expression to parse the CSV values.
+        var objPattern = new RegExp(
+            (
+                // Delimiters.
+                "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+                // Quoted fields.
+                "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+                // Standard fields.
+                "([^\"\\" + strDelimiter + "\\r\\n]*))"
+            ),
+            "gi"
+            );
+        // Create an array to hold our data. Give the array
+        // a default empty first row.
+        var arrData = [[]];
+        // Create an array to hold our individual pattern
+        // matching groups.
+        var arrMatches = null;
+        // Keep looping over the regular expression matches
+        // until we can no longer find a match.
+        while (arrMatches = objPattern.exec( strData )){
+            // Get the delimiter that was found.
+            var strMatchedDelimiter = arrMatches[ 1 ];
+            // Check to see if the given delimiter has a length
+            // (is not the start of string) and if it matches
+            // field delimiter. If id does not, then we know
+            // that this delimiter is a row delimiter.
+            if (
+                strMatchedDelimiter.length &&
+                (strMatchedDelimiter != strDelimiter)
+                ){
+                // Since we have reached a new row of data,
+                // add an empty row to our data array.
+                arrData.push( [] );
+            }
+            // Now that we have our delimiter out of the way,
+            // let's check to see which kind of value we
+            // captured (quoted or unquoted).
+            if (arrMatches[ 2 ]){
+                // We found a quoted value. When we capture
+                // this value, unescape any double quotes.
+                var strMatchedValue = arrMatches[ 2 ].replace(
+                    new RegExp( "\"\"", "g" ),
+                    "\""
+                    );
+            } else {
+                // We found a non-quoted value.
+                var strMatchedValue = arrMatches[ 3 ];
+            }
+            // Now that we have our value string, let's add
+            // it to the data array.
+            arrData[ arrData.length - 1 ].push( strMatchedValue );
+        }
+        // Return the parsed data.
+        return( arrData );
+    }
 
 function loadFile(filePath) {
 	var result = null;
@@ -127,8 +167,10 @@ function initialize() {
 	var happyEventsRows = loadFile("./assets/happy.csv").split("\n");
 
 	for (var i = 0; i < monthlyEventsRows.length-1; i++) {
-		var terms = CSVtoArray(monthlyEventsRows[i]);
+		var terms = CSVtoArray(monthlyEventsRows[i], ",");
 		var options = [];
+		console.log(i)
+		console.log(terms)
 		for (var j = 0; j < 3; j++) {
 			var change = new Change(parseInt(terms[j * 5 + 4]), parseInt(terms[j * 5 + 5]), parseInt(terms[j * 5 + 6]));
 			options.push(new Option(terms[j * 5 + 3], change, terms[j * 5 + 7]));
@@ -138,7 +180,7 @@ function initialize() {
 	}
 
 	for (var i = 0; i < happyEventsRows.length-1; i++) {
-		var terms = CSVtoArray(happyEventsRows[i]);
+		var terms = CSVtoArray(happyEventsRows[i], ",");
 		var change = new Change(parseInt(terms[4]), 0, parseInt(terms[5]));
 		happyEvents[terms[0]] = new HappyEvent(parseInt(terms[1]), terms[2], terms[3], change, terms[6]);
 	}
